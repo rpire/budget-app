5bea0ae9199f72153851acbd94859b86d5f2d28d
rpire <rubenpire7@gmail.com>
2022-01-04 19:42:23 -0400

    Set the application routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'splash#index'
+
+  resources :groups, only: [:index, :new, :show, :create]
+  resources :entities, only: [:new, :show, :create]
+
+  get 'completed', to: 'entities#completed'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
