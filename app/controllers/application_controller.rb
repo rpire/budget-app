59802f1d84c8a05d6a454c42c89aeb47f0afdfce
rpire <rubenpire7@gmail.com>
2022-01-04 21:36:15 -0400

    Implemented sign in and out redirections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    groups_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
   protected
 
   def update_allowed_parameters
