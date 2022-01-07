247bf00238aff993a22fdcfd7512f15d50b63735
rpire <rubenpire7@gmail.com>
2022-01-06 20:39:55 -0400

    Apply fix for rubocop errors

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -3,5 +3,5 @@ class Entity < ApplicationRecord
   has_many :group_entities
 
   validates :name, presence: true
-  validates :amount, presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
